8a523f3adb2647754a59328f37d74a7964b7aac4
harshafyniti <sriharsha.bakku@fyniti.com>
2020-04-12 04:48:47 +0530

    Exmo Data Preparation last 1 year 30 min data from chart using for loops and epochtimes

--- src/main/java/crypto/arbitOpp/DataPreparationExmo.java
+++ src/main/java/crypto/arbitOpp/DataPreparationExmo.java
@@ -11,6 +11,8 @@ import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
@@ -31,19 +33,56 @@ import com.opencsv.CSVWriter;
 public class DataPreparation {
 	public static void main(String[] args) throws Exception {
 
+		String filePath = "./exmo_USDT_USD.csv";
+		File file = new File(filePath); 
+		try { 
+			// create FileWriter object with file as parameter 
+			FileWriter outputfile = new FileWriter(file); 
 
-//		 JSONObject binanceData = getBinanceChartData();
-			JSONObject exmoData = getExmoChartData();
+			// create CSVWriter object filewriter object as parameter 
+			CSVWriter writer = new CSVWriter(outputfile); 
+
+			// adding header to csv 
+			String[] header = { "c", "t", "v", "h", "l", "o" }; 
+			writer.writeNext(header); 
+			
+		
+
+		Long  currentEpoch = Calendar.getInstance().getTimeInMillis();
+		int resolution = 30; // in minutes
+        int candlesNo = 3000;
+        int duration = 10;
+        int   fromEpoch = (int) ((currentEpoch/1000 - duration*candlesNo*resolution*60));
+        String to = new String();
+        String from = String.valueOf(fromEpoch);
+        for(int i=0; i<=10;i++) {
+		//String to = String.valueOf((int) (currentEpoch/1000));
+            if(!to.isEmpty()) {from = to;}		
+        	to = String.valueOf(Integer.valueOf(from)+candlesNo*resolution*60);
+		     
+			JSONObject exmoData = getExmoChartData(from,to,String.valueOf(resolution));
+
 			
 			ArrayList<JSONObject> candles = (ArrayList<JSONObject>) exmoData.get("candles");
 			
-			writeDataLineByLine("./USDT_USD.csv", candles);
-
-		
+			System.out.println("candle count: "+ candles.size());
+			
+			writeDataLineByLine(filePath, candles, writer);
+        }
+//        writer.close(); 
+		}catch (IOException e) { 
+			// TODO Auto-generated catch block 
+			e.printStackTrace(); 
+		} 
 	}
 	
-	public static JSONObject getExmoChartData() throws Exception {
-		String url = "https://chart.exmoney.com/ctrl/chart/history?symbol=USDT_USD&resolution=30&from=1579815298&to=1584999299";
+	public static JSONObject getExmoChartData(String from, String to, String resolution) throws Exception {
+	    //3000 candles bulk fetch allowed
+
+		String url = "https://chart.exmoney.com/ctrl/chart/history?symbol=USDT_USD&resolution=" + resolution
+				+ "&from=" + from + "&to=" + to;
+		
+		System.out.println(url);
 		URL obj = new URL(url);
 		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
 		con.setRequestMethod("GET");
@@ -83,42 +122,42 @@ public class DataPreparation {
 		return jsonObject;
 	}
 	
-	public static void writeDataLineByLine(String filePath, ArrayList<JSONObject> candles) 
+	public static void writeDataLineByLine(String filePath, ArrayList<JSONObject> candles, CSVWriter writer) 
 	{ 
 		// first create file object for file placed at location 
 		// specified by filepath 
-		File file = new File(filePath); 
-		try { 
+//		File file = new File(filePath); 
+//		try { 
 			// create FileWriter object with file as parameter 
-			FileWriter outputfile = new FileWriter(file); 
-
-			// create CSVWriter object filewriter object as parameter 
-			CSVWriter writer = new CSVWriter(outputfile); 
+//			FileWriter outputfile = new FileWriter(file); 
+//
+//			// create CSVWriter object filewriter object as parameter 
+//			CSVWriter writer = new CSVWriter(outputfile); 
 
 			// adding header to csv 
-			String[] header = { "c", "t", "v", "h", "l", "o" }; 
-			writer.writeNext(header); 
-
+			//String[] header = { "c", "t", "v", "h", "l", "o" }; 
+			//writer.writeNext(header); 
+            
 			
 			//System.out.println(candles);
 			for(JSONObject candle : candles) {
-				System.out.println(candle);
-				String[] data1 = { String.valueOf(candle.get("c")), String.valueOf(candle.get("t"))
+				//System.out.println(candle);
+				String[] data1 = { String.valueOf(candle.get("c")), String.valueOf(((Long)candle.get("t"))/1000)
 						,String.valueOf(candle.get("v")),String.valueOf(candle.get("h")),String.valueOf(candle.get("l")),String.valueOf(candle.get("o"))
 						}; 
-				writer.writeNext(data1); 
+				writer.writeNext(data1, false); 
 			
 			}
 			
 			
 
 			// closing writer connection 
-			writer.close(); 
-		} 
-		catch (IOException e) { 
-			// TODO Auto-generated catch block 
-			e.printStackTrace(); 
-		} 
+//			writer.close(); 
+//		} 
+//		catch (IOException e) { 
+//			// TODO Auto-generated catch block 
+//			e.printStackTrace(); 
+//		} 
 	} 
 
 
