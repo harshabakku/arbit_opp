001ee2d6235778498710e183f7f51552c1eba668
harshabakku <bakkubaku@gmail.com>
2020-06-17 10:28:58 +0530

    handle & in url and fix buy sell ratio

--- src/main/java/nifty/options/NiftyFOGainersLosers.java
+++ src/main/java/nifty/options/NiftyFOGainersLosers.java
@@ -10,6 +10,7 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
+import java.text.DecimalFormat;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -31,7 +32,7 @@ public class NiftyFOGainersLosers {
 
         while(true) {		
         	printNiftyData();
-        	TimeUnit.SECONDS.sleep(22);
+        	TimeUnit.SECONDS.sleep(122);
         }
 	}
 
@@ -71,20 +72,23 @@ public class NiftyFOGainersLosers {
 	private static void prettyPrintNiftyFOData(JSONArray dataArray) throws Exception {
 		Iterator<JSONObject> iterator = dataArray.iterator();
 
-		System.out.println(" percentChange buyQ sellQ buy/sell>2<0.5 previousPrice openPrice highPrice lowPrice ltp tradedQuantity symbol ");
+		System.out.println(" percentChange buy/sell>2<0.5  buyQ    sellQ   previousPrice openPrice highPrice lowPrice ltp tradedQuantity symbol ");
 		while (iterator.hasNext()) {
 			JSONObject dataObj = iterator.next();
 //			System.out.println(dataObj);
 			
 			JSONObject depthData = getDepthChartData(String.valueOf(dataObj.get("symbol")));
-			Double buySellRatio;
+			float buySellRatio;
 			Object symbol = dataObj.get("symbol");
-			Object totalSellQuantity = depthData.get("totalSellQuantity");
-			Object totalBuyQuantity = depthData.get("totalBuyQuantity");
+//			System.out.println(depthData);
+			Long totalSellQuantity = (Long) depthData.get("totalSellQuantity");
+			Long totalBuyQuantity = (Long) depthData.get("totalBuyQuantity");
 			try {
-			buySellRatio = (double) ((Long)totalBuyQuantity/(Long)depthData.get("totalSellQuantity"));
+			buySellRatio = ((float)totalBuyQuantity/totalSellQuantity);
+			DecimalFormat decimalFormat = new DecimalFormat("#.##");
+			buySellRatio = Float.valueOf(decimalFormat.format(buySellRatio));
 			}catch(Exception e) {
-				buySellRatio = 0.0;
+				buySellRatio = 0.0F;
 			}
 			String filePath= "./depthChartData/" + new Date().getDate()+ "/"+ symbol + ".csv";
 
@@ -103,7 +107,7 @@ public class NiftyFOGainersLosers {
 			// TODO Auto-generated catch block 
 			e.printStackTrace(); 
 		    } 			
-			System.out.println( "      "+ dataObj.get("netPrice") + "      "+ totalBuyQuantity+ "      "+ totalSellQuantity+ "      "+ buySellRatio + "      "+ dataObj.get("previousPrice")+ "      "+ dataObj.get("openPrice")+ "      "+ dataObj.get("highPrice")+ "      "+ dataObj.get("lowPrice") + "      "+ dataObj.get("ltp")+ "      "+ dataObj.get("tradedQuantity") + "      "+ symbol);
+			System.out.println( "      "+ dataObj.get("netPrice") + "      "+ buySellRatio + "      " + totalBuyQuantity+ "      "+ totalSellQuantity+ "      "+ dataObj.get("previousPrice")+ "      "+ dataObj.get("openPrice")+ "      "+ dataObj.get("highPrice")+ "      "+ dataObj.get("lowPrice") + "      "+ dataObj.get("ltp")+ "      "+ dataObj.get("tradedQuantity") + "      "+ symbol);
 			
 
 		}
@@ -113,7 +117,10 @@ public class NiftyFOGainersLosers {
 
 	private static JSONObject getDepthChartData(String symbol) throws MalformedURLException, ProtocolException, UnsupportedEncodingException, IOException, ParseException {
 		
-		
+		if (symbol.contains("&")) {
+			//url already has & to separate params
+			symbol = symbol.replace("&", "%26");
+		}
         String url = "https://www.nseindia.com/api/quote-equity?symbol="+symbol  + "&section=trade_info";		
 //		System.out.println(url);
 		JSONObject jsonObject = getResponse(url);
